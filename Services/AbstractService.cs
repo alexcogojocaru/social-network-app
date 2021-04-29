0ad0ca0fc5f6a128448c7af7b222e7c87e051811
Alex Cojocaru <alex.cojocaru73@gmail.com>
2021-04-29 15:58:30 +0300

    fixing the build because of the database connector

--- Services/AbstractService.cs
+++ Services/AbstractService.cs
@@ -8,7 +8,7 @@ namespace Services
 
         protected AbstractService()
         {
-            databaseConnector = DatabaseConnector.GetInstance();
+            //databaseConnector = DatabaseConnector.GetInstance();
         }
     }
 }
